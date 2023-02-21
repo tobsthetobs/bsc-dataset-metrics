424548f865dc2cfcf5288751812a2b1c26faa936
tobsthetobs <73800087+tobsthetobs@users.noreply.github.com>
2023-02-21 17:30:17 +0100

    Week3

--- BP_ratio.py
+++ BP_ratio.py
@@ -2,8 +2,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
-import rosbag
-from cv_bridge import CvBridge
 # import pandas as pd
 import os
 # import tensorflow as tf
@@ -22,9 +20,10 @@ im = imread('Datasets/EuRoC/cam0/1403638127245096960.png')
 dataset_folder = 'Datasets'
 
 
-## This section is for functions to load datasets and process accordingly
 
-# Function to iterate over the EuRoC dataset specifically.
+
+## This section is for functions to load datasets and process accordingly
+# Function to iterate over the EuRoC dataset specifically. This dataset is shot using a monochrome camera so will use the threshold_image() function to gather metrics
 def load_euroc_dataset(supress_output: bool):
     # Setup directories using os
     img_folder = 'EuRoC/'
@@ -32,11 +31,14 @@ def load_euroc_dataset(supress_output: bool):
     dir = cur_dir + "/" + dataset_folder + "/" + img_folder
     data = os.listdir(dir)
 
-    # Setup empty arrays to store data
+    # Setup empty lists to store data
     data_mean = [[], []]
     data_BR = [[], []]
     axis = 0
     counter = 0
+    sum = 0
+    delta = 0
+    prev = 0
 
     # Iterate over dataset
     for subfolder in data:
@@ -47,16 +49,27 @@ def load_euroc_dataset(supress_output: bool):
             # This is here for debugging
             if ((counter % 100) == 0) & (not (supress_output)):
                 print(counter)
+                
             # Dataset images are already gray scale by the looks of it so can run just take mean.
             mean = np.mean(image)
             BP, DP, _ = threshold_image(image, False)
             data_mean[axis].append(mean)
             data_BR[axis].append(BP/DP)
+            
+            # Extract largest change in mean intensity of image:
+            if counter == 0:
+                prev = mean
+                delta = 0
+            if (delta < abs(mean - prev)):
+                delta = abs(mean - prev)
+        sum += counter
+        print("Scanning next folder current total of images processed: ", sum)
+        counter = 0 
         axis += 1
-    return data_BR, data_mean
+    return data_BR, data_mean, delta
 
-# Function to iterate over the AQUALOC dataset specifically
-# aqualoc is packed as a bag so will use/modify function from https://gist.github.com/wngreene/835cda68ddd9c5416defce876a4d7dd9 to suit this function
+# Function to iterate over the AQUALOC dataset specifically, photos are already in gray scale.
+# Function calculates mean image intensity, doesnt caclulate values from binary image.
 def load_aqualoc_dataset(supress_output: bool):
     # Setup directories using os 
     img_folder = 'AQUALOC/'
@@ -64,21 +77,41 @@ def load_aqualoc_dataset(supress_output: bool):
     dir = cur_dir + "/" + dataset_folder + "/" + img_folder
     data = os.listdir(dir)
     
-    # Need to find a way to load data from a .bag file
+    # Setup lists to store data
+    data_brightness = []
+    counter = 0
     
-    
-    return 0
+    # Iterate over dataset
+    for subfolder in data:
+        sequence = []
+        for file in os.listdir(os.path.join(dir,subfolder)):
+            image = imread(os.path.join(dir, subfolder, file))
+            counter += 1
+            
+            # Check number of image channels, convert to HSV if not a grayscale image.
+            if check_image_dim(image):
+                im_hsv = load_to_colorspace(image, "HSV")
+                sequence.append(np.mean(im_hsv))
+            else:
+                sequence.append(np.mean(image))
+            
+        data_brightness.append(sequence)
+    return data_brightness
+
+
 
 ## Section for algorithmic functions
-
 # Make function to process image to correct HSV / YUV color space
 def load_to_colorspace(image, COLORSPACE: str):
     assert COLORSPACE == 'HSV' or COLORSPACE == 'YUV', f'ERROR: Wrong COLORSPACE name selected. COLORSPACE should be one of "HSV", "YUV"'
     if COLORSPACE == 'HSV':
         img = cv2.cvtColor(image, cv2.COLOR_RGB2HSV_FULL)
+        # Return only value channel
+        return img[:,:,2]
     elif COLORSPACE == 'YUV':
         img = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
-    return img
+        # Return only luminace channel
+        return img[:,:,0]
 
 # Create thresholded picture. To take either 0 or 255 in pixel value.
 def threshold_image(image, include: bool):
@@ -86,9 +119,7 @@ def threshold_image(image, include: bool):
     brightPixCounter = 0
     dimPixCounter = 0
 
-    # Check if image has multiple channels:
-    if image.ndim < 1:
-        # Convert to gray-scale
+    if check_image_dim(image):
         imgs = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     else:
         imgs = image
@@ -109,9 +140,37 @@ def threshold_image(image, include: bool):
     return brightPixCounter, dimPixCounter, False
 
 
+def colorspace_threshold_image(image, COLORSPACE: str, include: bool):
+    # Initialize locals:
+    brightPixCounter = 0
+    dimPixCounter = 0
+    
+    imgs = load_to_colorspace(image, COLORSPACE)
+
+    imbw = imgs > imgs.mean()
+    imbw = img_as_ubyte(imbw)
+
+    # Flatten image
+    imbw_flt = copy(imbw)
+    imbw_flt.flatten()
+    _, counts = np.unique(imbw_flt, return_counts=True)
+    dimPixCounter = counts[0]
+    brightPixCounter = counts[1]
+
+    if include:
+        return brightPixCounter, dimPixCounter, imbw
+
+    return brightPixCounter, dimPixCounter, False
+
+def check_image_dim(image):
+    if image.ndim > 2:
+        return True
+    else:
+        return False
+
+
+
 ## Section for utility functions for such as printing information etc.
-
-
 # Function to print out current processed image. Specifically for use if include = True is passed to function threshold_image
 def print_out(threshold_tuple):
     BP, DP, im = threshold_tuple
@@ -133,8 +192,27 @@ def feedman_bins(data):
     bins = round((max(data) - min(data)) / bin_width)
     return bins
 
-# Function to plot histograms
+# Function to plot histograms for EuRoC
 def create_histogram_euroc(data_tuple):
+    data_br, data_m, _ = data_tuple
+    f, ax = plt.subplots(2, 2)
+    sns.histplot(data_br[0], bins=feedman_bins(
+        data_br[0]), kde=True, ax=ax[0, 0])
+    ax[0, 0].set_title("pixel ratio cam0")
+    sns.histplot(data_m[0], bins=feedman_bins(
+        data_m[0]), kde=True, ax=ax[0, 1])
+    ax[0, 1].set_title("Mean image intensity cam0")
+    sns.histplot(data_br[1], bins=feedman_bins(
+        data_br[1]), kde=True, ax=ax[1, 0])
+    ax[1, 0].set_title("pixel ratio cam1")
+    sns.histplot(data_m[1], bins=feedman_bins(
+        data_m[1]), kde=True, ax=ax[1, 1])
+    ax[1, 1].set_title("Mean image intensity cam1")
+    plt.tight_layout()
+    plt.show()
+    
+# Function to plot histograms for AQUALOC
+def create_histogram_aqualoc(data_tuple):
     data_br, data_m = data_tuple
     f, ax = plt.subplots(2, 2)
     sns.histplot(data_br[0], bins=feedman_bins(
@@ -154,7 +232,7 @@ def create_histogram_euroc(data_tuple):
 
 # Function used for printing all metrics on downloaded EuRoC dataset
 def print_euroc_metrics(data_tuple, include: bool):
-    data_br, data_m = data_tuple
+    data_br, data_m, delta = data_tuple
     
     # Calculate metrics
     br0 = np.mean(data_br[0])
@@ -188,12 +266,13 @@ def print_euroc_metrics(data_tuple, include: bool):
     print("Combined pixel ratio variance on both cams: ", var_br_comb)
     print("Combined mean image intensity of both cams: ", m_comb)
     print("Combined mean image intensity variance of both cams: ", var_m_comb)
+    print("Largest delta: ", delta)
 
 # Function used to return all metrics for downloaded EuRoC dataset,
 # returns a tuple with 4 values (br_comb, var_br_comb, m_comb, var_m_comb) which corresponds to: (brightness ratio combined, brightness ratio variance, mean image intensity mean, mean image intensity variance)
 # if include is passed as True function will then pass 2 arrays containing the metrics for each cam.
 def euroc_metrics(data_tuple, include: bool):
-    data_br, data_m = data_tuple
+    data_br, data_m, delta = data_tuple
     
     # Calculate metrics
     br0 = np.mean(data_br[0])
@@ -216,5 +295,4 @@ def euroc_metrics(data_tuple, include: bool):
         means = [br0, br1, m0, m1]
         vars = [br0_var, br1_var, m0_var, m1_var]
         return (br_comb, var_br_comb, m_comb, var_m_comb), means, vars
-    return (br_comb, var_br_comb, m_comb, var_m_comb)
-
+    return (br_comb, var_br_comb, m_comb, var_m_comb, delta)
